ad1df04dc70ac9108f335442b44c3284b300c1ff
alex9507 <28487409+alex9507@users.noreply.github.com>
2019-12-12 17:37:22 +0200

    Added Payment for chalets.

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PaypalExpressCheckout.BusinessLogic.Interfaces;
+using Stripe;
 
 namespace MountainTourismBookingSystem.Controllers
 {
     public class PaymentController : Controller
     {
-        private readonly IPaypalServices _PaypalServices;
-
-        public PaymentController(IPaypalServices paypalServices)
-        {
-            _PaypalServices = paypalServices;
-        }
+        // private readonly IPaypalServices _PaypalServices;
+        // 
+        // public PaymentController(IPaypalServices paypalServices)
+        // {
+        //     _PaypalServices = paypalServices;
+        // }
 
         public IActionResult Index()
         {
@@ -22,22 +23,60 @@ namespace MountainTourismBookingSystem.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult CreatePayment()
+        public IActionResult Charge(string stripeEmail, string stripeToken)
         {
-            var payment = _PaypalServices.CreatePayment(100, "http://localhost:44385/Payment/ExecutePayment", "http://localhost:44385/Payment/Cancel", "sale");
+            var customers = new CustomerService();
+            var charges = new ChargeService();
 
-            return new JsonResult(payment);
+            var customer = customers.Create(new CustomerCreateOptions
+            {
+                Email = stripeEmail,
+                Source = stripeToken
+            });
+
+            var charge = charges.Create(new ChargeCreateOptions
+            {
+                Amount = 1111,
+                Description = "Test Payment",
+                Currency = "usd",
+                Customer = customer.Id,
+                ReceiptEmail = stripeEmail,
+                Metadata = new Dictionary<string, string>()
+                {
+                    { "OrderId", "111" },
+                    { "Postcode", "LEE111"},
+                }
+            });
+
+            if (charge.Status == "succeeded")
+            {
+                string BalanceTransactionId = charge.BalanceTransactionId;
+                return View();
+            }
+            else
+            {
+
+            }
+
+            return View();
         }
 
-        [HttpPost]
-        public IActionResult ExecutePayment(string paymentId, string token, string PayerID)
-        {
-            var payment = _PaypalServices.ExecutePayment(paymentId, PayerID);
-
-            // Hint: You can save the transaction details to your database using payment/buyer info
-
-            return Ok();
-        }
+        // [HttpPost]
+        // public IActionResult CreatePayment()
+        // {
+        //     var payment = _PaypalServices.CreatePayment(100, "http://localhost:44385/Payment/ExecutePayment", "http://localhost:44385/Payment/Cancel", "sale");
+        // 
+        //     return new JsonResult(payment);
+        // }
+        // 
+        // [HttpPost]
+        // public IActionResult ExecutePayment(string paymentId, string token, string PayerID)
+        // {
+        //     var payment = _PaypalServices.ExecutePayment(paymentId, PayerID);
+        // 
+        //     // Hint: You can save the transaction details to your database using payment/buyer info
+        // 
+        //     return Ok();
+        // }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace MountainTourismBookingSystem
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            StripeConfiguration.ApiKey = Configuration.GetSection("Stripe")["PublishableKey"];
+            StripeConfiguration.ApiKey = Configuration.GetSection("Stripe")["SecretKey"];
 
             if (env.IsDevelopment())
             {
