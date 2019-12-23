31c935ce3ee2230d92c88fd1da1bff8017856be2
alex9507 <28487409+alex9507@users.noreply.github.com>
2019-12-23 17:00:49 +0200

    Adding All pages for Account Management

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MountainTourismBookingSystem.Data;
+
+[assembly: HostingStartup(typeof(MountainTourismBookingSystem.Areas.Identity.IdentityHostingStartup))]
+namespace MountainTourismBookingSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
