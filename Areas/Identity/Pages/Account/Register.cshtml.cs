5de6c94c5eabcfb4db9a22ed8a1266d4fbecbd4a
alex9507 <28487409+alex9507@users.noreply.github.com>
2019-12-30 03:26:03 +0200

    Fixed Personal Data loading view.

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -90,6 +90,8 @@ namespace MountainTourismBookingSystem.Areas.Identity.Pages.Account
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email", $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
+                    await _userManager.AddToRoleAsync(user, "User");
+
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
                 }
